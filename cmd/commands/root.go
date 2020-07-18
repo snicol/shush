4b9b7f130e36e82581cde0379af368036cd62592
Scott Nicol <s.cott@me.com>
2020-07-19 00:59:22 +0100

    use init + OnInitialize

--- cmd/commands/root.go
+++ cmd/commands/root.go
@@ -2,6 +2,7 @@ package commands
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	shush "shush/lib"
@@ -19,40 +20,53 @@ var (
 	sess *shush.Session
 
 	profile string
-	conf    Config
+	conf    *Config
 
 	rootCmd = &cobra.Command{
 		Use:   "shush",
 		Short: "Shush is an AWS secret manager tool. See `shush --help`.",
+		Run:   func(cmd *cobra.Command, args []string) {},
 	}
 )
 
-func Execute() {
+func init() {
+	cobra.OnInitialize(initConfig)
+
 	rootCmd.PersistentFlags().StringVarP(&profile, "profile", "p", "default", "Config profile to use")
 
+	rootCmd.AddCommand(getCmd)
+	rootCmd.AddCommand(setCmd)
+	rootCmd.AddCommand(syncCmd)
+}
+
+func initConfig() {
 	viper.SetConfigName("config")
 	viper.SetConfigType("yaml")
 	viper.AddConfigPath("$HOME/.shush/")
 
 	err := viper.ReadInConfig()
 	if err != nil {
-		panic(fmt.Errorf("Fatal error config file: %s", err))
+		log.Fatalf("Fatal error config file: %s", err)
 	}
 
 	err = viper.UnmarshalKey(profile, &conf)
 	if err != nil {
-		panic(fmt.Errorf("unmarshal config file failed: %s", err))
+		log.Fatalf("unmarshal config file failed: %s", err)
+	}
+
+	if conf == nil {
+		log.Fatalf("no profile %s specified in config", profile)
 	}
 
 	storageProvider := getStorageProvider(conf.Storage)
 	cacheProvider := getCacheProvider(conf.Cache)
 
+	log.Println(cacheProvider)
+
 	sess = shush.NewSession(storageProvider, cacheProvider, shush.UpsertVersionReplaceNewer)
+}
 
-	rootCmd.AddCommand(getCmd)
-	rootCmd.AddCommand(setCmd)
-	rootCmd.AddCommand(syncCmd)
-
+func Execute() {
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -68,12 +82,13 @@ func getStorageProvider(storageConf StorageConfig) storage.Provider {
 			SharedConfigState:       session.SharedConfigEnable,
 		})
 		if err != nil {
-			panic(err)
+			log.Fatal(err)
 		}
 
 		return storage.NewPMSKMS(sess, storageConf.Config.KeyID)
 	default:
-		panic(fmt.Sprintf("unknown storage provider type %s", storageConf.Type))
+		log.Fatal(fmt.Sprintf("unknown storage provider type %s", storageConf.Type))
+		return nil
 	}
 }
 
@@ -91,6 +106,7 @@ func getCacheProvider(cacheConf *CacheConfig) cache.Provider {
 
 		return cache.NewKeychain(secClass, cacheConf.Config.Service, cacheConf.Config.AccessGroup)
 	default:
-		panic(fmt.Errorf("unknown cache provider type %s", cacheConf.Type))
+		log.Fatalf("unknown cache provider type %s", cacheConf.Type)
+		return nil
 	}
 }
